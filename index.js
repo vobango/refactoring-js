eb2cd062543b0598418c0f1a032e54e4ac4acfff
Kaspar Arme <kaspar.arme@gmail.com>
2018-06-27 07:05:05 +0300

    Remove setup

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var classifier = {
-    setup: function () {
-        this.songs = [];
-        this.allChords = new Set();
-        this.labelCounts = new Map();
-        this.labelProbabilities = new Map();
-        this.chordCountsInLabels = new Map();
-        this.probabilityOfChordsInLabels = new Map();
-    }
+    songs: [],
+    allChords: new Set(),
+    labelCounts: new Map(),
+    labelProbabilities: new Map(),
+    chordCountsInLabels: new Map(),
+    probabilityOfChordsInLabels: new Map(),
 };
 
 function fileName() {
@@ -84,7 +82,6 @@ function setProbabilityOfChordsInLabels() {
 }
 
 function trainAll() {
-    classifier.setup();
     setDifficulties();
     setSongs();
     train(imagine, easy);
